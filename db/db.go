a36a3dd22c4cd633bddc01ed719b9afc454455e2
Quang Ha <quanghahl315@gmail.com>
2022-06-22 20:31:39 +0700

    structure and implement few function

--- db/db.go
+++ db/db.go
@@ -3,22 +3,19 @@
 package db
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/go-pg/pg"
 )
 
 func Connect() *pg.DB {
-	connStr := os.Getenv("DB_URL")
-	opt, err := pg.ParseURL(connStr)
-	if err != nil {
-		panic(err)
-	}
-	db := pg.Connect(opt)
 
-	if _, DBStatus := db.Exec("SELECT 1"); DBStatus != nil {
-		fmt.Println(db)
+	db := pg.Connect(&pg.Options{
+		Addr:     "127.0.0.1:5432",
+		User:     "postgres",
+		Database: "gobooks",
+		Password: "AElamot2",
+	})
+	_, DBStatus := db.Exec("SELECT 1")
+	if DBStatus != nil {
 		panic("PostgreSQL is down")
 	}
 	return db
